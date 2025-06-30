f44736d6347335ee3e624102051f8b14b5234fa0
Augustine-chukwuma <augustinechukwumanwafor@gmail.com>
2025-06-30 04:33:40 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 // Fallback: send index.html for unmatched routes (for SPA routing)
 app.get('*', (req, res) => {
