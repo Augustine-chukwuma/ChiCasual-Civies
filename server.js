85aa994b07efd236ab7b7866b7b868bf631a3fce
Augustine-chukwuma <augustinechukwumanwafor@gmail.com>
2025-07-04 06:06:56 -0700

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(''));
+app.use(express.static('./'));
 
 // Cloudinary Configuration with env variables
 cloudinary.config({
